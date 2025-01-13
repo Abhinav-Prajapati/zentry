5c4630f9830a2e671704340f6b8caff56368390b
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2025-01-13 15:36:55 +0530

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { About } from './components/About'
 import { Features } from './components/Features'
 import Hero from './components/Hero'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <main className='relative min-h-screen w-screen'>
+      <Navbar />
       <Hero />
       <About />
       <Features />

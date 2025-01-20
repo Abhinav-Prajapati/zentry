98c2b1825f1f3563c76b6e25bb79570e43d53e6d
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2025-01-20 21:10:32 +0530

    add cards to website

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { About } from './components/About'
 import Features from './components/Features'
+import Footer from './components/Footer';
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Story from "./components/Story";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Hero />
       <About />
       <Features />
+      <Story />
+      <Footer />
     </main>
   )
 }
